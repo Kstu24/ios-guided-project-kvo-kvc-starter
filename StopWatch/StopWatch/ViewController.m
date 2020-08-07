be23e4ed2564a158442db94d21da5dfe4ecfb483
Kevin Stewart <kistewart419@gmail.com>
2020-08-06 23:10:41 -0400

    Cleaned up old stop watch observations

--- StopWatch/StopWatch/ViewController.m
+++ StopWatch/StopWatch/ViewController.m
@@ -11,7 +11,7 @@
 
 
 // TODO: Create a KVOContext to identify the StopWatch observer
-
+void *KVOContext = &KVOContext;
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *timeLabel;
@@ -72,22 +72,55 @@
         
         // willSet
 		// TODO: Cleanup KVO - Remove Observers
+        [_stopwatch removeObserver:self
+                        forKeyPath:@"runner"
+                           context:KVOContext];
+        [_stopwatch removeObserver:self
+                        forKeyPath:@"elapsedTime"
+                           context:KVOContext];
 
         _stopwatch = stopwatch;
         
         // didSet
 		// TODO: Setup KVO - Add Observers
+        [_stopwatch addObserver:self
+                     forKeyPath:@"running"
+                        options:0
+                        context:KVOContext];
+        [_stopwatch addObserver:self
+                     forKeyPath:@"elapsedTime"
+                        options:0
+                        context:KVOContext];
     }
     
 }
 
-
 // TODO: Review docs and implement observerValueForKeyPath
 
+-(void)observeValueForKeyPath:(NSString *)keyPath
+                     ofObject:(id)object
+                       change:(NSDictionary<NSKeyValueChangeKey,id> *)change
+                      context:(void *)context
+{
+    if (context == KVOContext) {
+        if ([keyPath isEqualToString:@"running"]) {
+            NSLog(@"Update the UI! Running: %@", (self.stopwatch.running ? @"YES" : @"NO"));
+            [self updateViews];
+        } else if ([keyPath isEqualToString:@"elapsedTime"]) {
+            NSLog(@"Update the UI! Elapsed Time: %.2f", (self.stopwatch.elapsedTime));
+            [self updateViews];
+        }
+    } else {
+        [super observeValueForKeyPath:keyPath
+                             ofObject:object
+                               change:change
+                              context:context];
+    }
+}
 
 - (void)dealloc {
-	// TODO: Stop observing KVO (otherwise it will crash randomly)
-    
+	// TODO: Stop observing KVO (otherwise it will crash randomly when the VC is no longer on screen)
+    self.stopwatch = nil;
 }
 
 @end
